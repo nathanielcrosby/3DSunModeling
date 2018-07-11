2e7928e58fc641043021b656919c5db8773b4bfb
Nathaniel Crosby <nathanielcrosby@verizon.net>
2018-07-11 13:03:33 -0400

    working 3D wrapping

--- 3Dplotting/3Dplot.py
+++ 3Dplotting/3Dplot.py
@@ -2,43 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from mpl_toolkits.mplot3d import Axes3D
-import sunpy.io as io
+#import sunpy.io as io
 from matplotlib._png import read_png
 from PIL import Image
 
-data = io.read_file('2007_02_02__12_02_58_608__HINODE_XRT_COMP.jp2', filetype='jp2')
-header = io.read_file_header('2007_02_02__12_02_58_608__HINODE_XRT_COMP.jp2')
+#data = io.read_file('2007_02_02__12_02_58_608__HINODE_XRT_COMP.jp2', filetype='jp2')
+#header = io.read_file_header('2007_02_02__12_02_58_608__HINODE_XRT_COMP.jp2')
 
 headerList = []
 
 dist_earth_to_sun = 148000000 #in km, changes depending on time
 degree_per_arcsec = 1./3600. 
 rad_per_degree = np.pi/180
-square_km_per_pixel = (np.sin((degree_per_arcsec * rad_per_degree / 2))
- * dist_earth_to_sun * 2)**2
+km_per_pixel = (np.sin((degree_per_arcsec * rad_per_degree / 2))
+ * dist_earth_to_sun * 2)
+square_km_per_pixel = km_per_pixel**2
 
-XCEN = header[0].__getitem__('XCEN')
-YCEN = header[0].__getitem__('YCEN')
-DATEOBS = header[0].__getitem__('CTIME')
-NAXIS1 = header[0].__getitem__('NAXIS1')
-NAXIS2 = header[0].__getitem__('NAXIS2')
+#XCEN = header[0].__getitem__('XCEN')
+#YCEN = header[0].__getitem__('YCEN')
+#DATEOBS = header[0].__getitem__('CTIME')
+#NAXIS1 = header[0].__getitem__('NAXIS1')
+#NAXIS2 = header[0].__getitem__('NAXIS2')
 
-#image = data[0][0]
+#image = data[0][0] #gets the data from the jp2 file, however this format does not work with the the plot surface, instead it is converted to a png in an oustide program
 image = read_png('2007_02_02__12_02_58_608__HINODE_XRT_COMP.png')
 
+xDimen = image.shape[0]
+yDimen = image.shape[1]
+
+#Used for the 2d plane in a 3d diagram
 #x, y = np.mgrid[0:image.shape[0], 0:image.shape[1]]
 #z = np.zeros((image.shape[0], image.shape[1]))
 
-r = 900        #determines the size of the sphere
-pi = np.pi
-cos = np.cos
-sin = np.sin
-phi, theta = np.mgrid[0:pi/2.:1800j, 0:2.*pi:1800j] #it is pi/2 to create a hemisphere
+r = 900.        #determines the size of the sphere, should be radius of the sun in image
 
-#creates the 3d hemisphere to serve as the 'canvas' for the image
-x = r * sin(phi) * cos(theta)
-y = r * sin(phi) * sin(theta)
-z = r/2. * cos(phi)
+x, y = np.mgrid[0:xDimen, 0:yDimen]
+zlist = []
+
+for xpoint in range(xDimen):
+	xrow = []
+	for ypoint in range(yDimen):
+		if(np.sqrt((xpoint-(xDimen/2.))**2. + (ypoint-(yDimen/2.))**2.) >= r):
+			xrow.append(0)
+		else:
+			xrow.append(np.sqrt(r**2. - (xpoint-(xDimen/2.))**2. - (ypoint-(yDimen/2.))**2.))
+	zlist.append(xrow)
+
+z = np.asarray(zlist)
 
 fig = plt.figure(figsize=(10.,10.))
 
@@ -56,6 +66,11 @@ fig = plt.figure(figsize=(10.,10.))
 #2D drawing in a 3D plane
 ax2 = fig.add_subplot(111, projection='3d')
 
+#uniform scaling
+ax2.set_xlim3d(0, 2048)
+ax2.set_ylim3d(0, 2048)
+ax2.set_zlim3d(0, 2048)
+
 #rstride and cstride determine how frequently values are taken from the arrays and plotted, lower stride yields higher resolution
 #norm is the instance of the Normalize class that is used to map the inputted values to actual colors, this input is not needed
 #cmap is a color map for the surface patches. This line isn't necessary but makes the colors better
@@ -63,7 +78,7 @@ ax2 = fig.add_subplot(111, projection='3d')
 #	They then match up with the cmap inputted which is the same
 #antialiased determines whether or not the figure is drawn with antialiasing
 
-ax2.plot_surface(x, y, z, rstride=5, cstride=5, antialiased=True, cmap=plt.cm.jet,
-facecolors=plt.cm.jet(image))#, norm = norm)
+ax2.plot_surface(x, y, z, rstride=10, cstride=10, antialiased=True, cmap=plt.cm.gist_heat,
+facecolors=plt.cm.gist_heat(image))#, norm=norm)
 plt.show()
-#plt.savefig('3d.png')
+plt.savefig('3d.png')
