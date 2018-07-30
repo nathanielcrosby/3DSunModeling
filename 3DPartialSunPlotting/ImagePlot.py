f8604568d1aa93003383ffb7629ebf426dcc92e5
Nathaniel Crosby <nathanielcrosby@verizon.net>
2018-07-30 16:35:17 -0400

    Gaussian Filtering, using a png from jp2, earth to scale

--- 3DPartialSunPlotting/ImagePlot.py
+++ 3DPartialSunPlotting/ImagePlot.py
@@ -4,6 +4,7 @@ from scipy.misc import imresize
 from scipy.ndimage import gaussian_filter
 import numpy as np
 import matplotlib.pyplot as plt
+import sunpy.io as io
 
 def km_per_pixel(arcs_per_pix = 1.):
 	#this uses known values to approximate the number of km per pixel
@@ -15,20 +16,30 @@ def km_per_pixel(arcs_per_pix = 1.):
 	square_km_per_pixel = km_per_pixel**2.
 	return km_per_pixel
 	
-def stl_file_maker(data):
+def stl_file_maker(data, interval=1, threshold=0.):
 	'''This uses the stl_tools numpy2stl in order to convert an array into a 3D printable
 	model. This cannot take xyz dimensions and cannot make the full 3D model. It makes the
 	2D image 3D printable.'''
 	
-	data = data
-	#data = gaussian_filter(data, 2)
+	earth_radius_px = earth_radius / km_per_pixel
 	
-	data =  data[:, :, 0] + 1.*data[:,:, 1]
-	data = imresize(data, (data.shape[0], data.shape[1])) 
-	#data = gaussian_filter(data, 5)
+	for xpoint in range(data.shape[0]):
+		for ypoint in range(data.shape[1]):
+			if (np.sqrt((xpoint - earth_scale_x)**2 + (ypoint - earth_scale_y)**2) <= earth_radius_px):
+				data[xpoint][ypoint] = 0.1
+				data[xpoint][ypoint] = 0.1
+				data[xpoint][ypoint] = 0.1
+			elif (data[xpoint][ypoint] <= threshold):
+				data[xpoint][ypoint] = 0
+			else:
+				data[xpoint][ypoint] = (data[xpoint][ypoint] - threshold) / (1 - threshold)
+	
+	#data =  data[:, :, 0] + 1.*data[:,:, 1]
+	data = imresize(data, (data.shape[0]/interval, data.shape[1]/interval)) 
+	data = gaussian_filter(data, 1)
 	
 	numpy2stl(data, 'test.stl', scale=.2, solid=True, max_width=100, 
-	max_depth=100, max_height=1, force_python=True)	
+	max_depth=100, max_height=20, force_python=True)	
 	
 def TwoDPlot(fig):
 	#2D drawing of the image
@@ -38,17 +49,28 @@ def TwoDPlot(fig):
 	#plt.savefig('2d.png')
 	plt.show()
 
+earth_radius = 6371 #km
+
 scale_factor_percent = 0.3
 
 km_per_pixel = km_per_pixel(0.61)
-image = read_png('2014_05_27_14_38_35_AIA_171__AIA_304-2.png')
+#image = read_png('2014_05_27_14_38_35_AIA_171__AIA_304-2.png')
+
+#data = io.read_file('2014_05_27__14_38_31_12__SDO_AIA_AIA_304.jp2')
+	
+#header = io.read_file_header('2014_05_27__14_38_31_12__SDO_AIA_AIA_304.jp2')
+
+image = read_png('2014_05_27__14_38_31_12__SDO_AIA_AIA_304.png')
+
+earth_scale_x = 35
+earth_scale_y = image.shape[1] - 35 #px
 
 #creates figure
 fig = plt.figure(figsize=(10.,10.))
 
 #TwoDPlot(fig)
 
-stl_file_maker(image)
+stl_file_maker(image, interval=2)
 
 #closes all plots
 plt.close('all')
