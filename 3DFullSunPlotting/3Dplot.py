e7cdaa43cc22f38b970f4bba46988634677d46f7
Nathaniel Crosby <nathanielcrosby@verizon.net>
2018-08-02 13:05:03 -0400

    reorganizing

--- 3DFullSunPlotting/3Dplot.py
+++ 3DFullSunPlotting/3Dplot.py
@@ -38,10 +38,24 @@ def stl_file_maker(data, scale=100, width=100, depth=100, height=20):
 	numpy2stl(data, '~/Desktop/test.stl', scale=scale, solid=True, max_width=width, 
 	max_depth=depth, max_height=height)	
 
-def stl_mesh_maker(x, y, z, interval=2):
-	'''This uses the stl mesh.Mesh in order to turn the x, y, and z arrays into a single 
+def stl_mesh_maker(x, y, z, interval=2, fname='test1.stl'):
+	'''
+	This uses the stl mesh.Mesh in order to turn the x, y, and z arrays into a single 
 	array with 3 pts each. It then turns it into vectors by taking 2 adjacent points. Used
 	to make full 3D models. Interal determines the rate at which points in the array are used
+	
+	Parameters:
+	
+	x : x array
+	
+	y : y array
+	
+	z : z array
+	
+	interval: int, the rate at which values are parsed from the x, y, z, arrays in the
+	creation of the stl file, lower -> higher resolution
+		
+	fname: str, name of the stl file created
 	'''
 	
 	print('creating mesh...')
@@ -134,10 +148,10 @@ def stl_mesh_maker(x, y, z, interval=2):
 	data[data.shape[0]-1][data.shape[1]-1][2]]])
 	
 	new_mesh = mesh.Mesh(vector_data)
-	new_mesh.save('test1.stl')
+	new_mesh.save(fname)
 	return new_mesh
 
-def TwoDPlot(figx=10., figy=10.):
+def TwoDPlot(image, figx=10., figy=10., save=False, file='2d.png'):
 	'''2D drawing of the image'''
 	
 	print('2D plotting...')
@@ -147,11 +161,14 @@ def TwoDPlot(figx=10., figy=10.):
 	
 	ax = fig.add_subplot(1, 1, 1)
 	ax.imshow(image)
-	ax.imshow(image, cmap=plt.cm.gist_heat, origin='lower')
-	#plt.savefig('2d.png')
+	ax.imshow(image, cmap=plt.cm.hot, origin='lower')
+	
+	if(save):
+		plt.savefig(file)
+		
 	plt.show()
 
-def TwoDin3DPlot(image, figx=10., figy=10.):
+def TwoDin3DPlot(image, figx=10., figy=10., save=False, file='2din3d.png'):
 	'''Used for the 2d image on plane in a 3d diagram'''
 	
 	print('2D plot in 3D...')
@@ -165,9 +182,13 @@ def TwoDin3DPlot(image, figx=10., figy=10.):
 	ax = fig.add_subplot(111, projection='3d')
 	ax.plot_surface(x, y, z, rstride=10, cstride=10, antialiased=True, cmap=plt.cm.hot,
 	facecolors=plt.cm.hot(image))
+	
+	if(save):
+		plt.savefig(file)
+	
 	plt.show()
 
-def ThreeDPlot(x, y, z, image, stride=10, figx=10., figy=10., save=False):
+def ThreeDPlot(x, y, z, image, stride=10, figx=10., figy=10., save=False, file='3d.png'):
 	'''Plots the axis created above and allows for specification of initial angle, points,
 	color map etc.
 	'''
@@ -180,9 +201,9 @@ def ThreeDPlot(x, y, z, image, stride=10, figx=10., figy=10., save=False):
 	ax = fig.add_subplot(111, projection='3d')
 
 	#uniform scaling of axes so that hemisphere is not stretched
-	ax.set_xlim3d(0, 1500000)
-	ax.set_ylim3d(0, 1500000)
-	ax.set_zlim3d(0, 1500000)
+	ax.set_xlim3d(0, x.max())
+	ax.set_ylim3d(0, x.max())
+	ax.set_zlim3d(0, x.max())
 
 	#labels axes
 	ax.set_xlabel('km')
@@ -205,11 +226,11 @@ def ThreeDPlot(x, y, z, image, stride=10, figx=10., figy=10., save=False):
 	ax.view_init(elev=45, azim=45)
 	
 	if save:
-		plt.savefig('3d.png')
+		plt.savefig(file)
 		
 	plt.show()
 
-def make_movie(x, y, z):
+def make_movie(x, y, z, image, file='movie.gif', fps=30, st_ang=0, en_ang=360, st_elev_ang=90, en_elev_ang=0, time=10):
 	'''Calls the make movie rotanimate function and makes a move with the specified 
 	azim pts, elev pts, filename, and fps
 	'''
@@ -219,34 +240,33 @@ def make_movie(x, y, z):
 	ax = fig.add_subplot(111, projection='3d')
 
 	#uniform scaling of axes so that hemisphere is not stretched
-	ax.set_xlim3d(0, 1500000)
-	ax.set_ylim3d(0, 1500000)
-	ax.set_zlim3d(0, 1500000)
+	ax.set_xlim3d(0, x.max())
+	ax.set_ylim3d(0, x.max())
+	ax.set_zlim3d(0, x.max())
 	
 	#no visible axis, good for movie
 	plt.axis('off')
 	
-	ax.plot_surface(x, y, z, rstride=5, cstride=5, antialiased=True, cmap=plt.cm.jet,
-	facecolors=plt.cm.jet(image))
+	ax.plot_surface(x, y, z, rstride=5, cstride=5, antialiased=True, cmap=plt.cm.hot,
+	facecolors=plt.cm.hot(image))
 	
-	azim = np.linspace(0,360,300) # A list of angles between 0 and 360 rotation angle
-	elev = np.linspace(90,0,300) # A list of angles between 90 and 0 elevation angle
-	# create a movie with 10 frames per seconds and 'quality' 2000
-	file = 'movie.gif' #name of movie
+	azim = np.linspace(st_ang,en_ang,fps*time) # A list of angles between 0 and 360 rotation angle
+	elev = np.linspace(st_elev_ang,en_elev_ang,fps*time) # A list of angles between 90 and 0 elevation angle
 	#calling function to make a movie with set points
-	makeMovie.rotanimate(ax, file, azim, elev, fps=30)
+	makeMovie.rotanimate(ax, file, azim, elev, fps=fps)
 
-def log_scale(intensity):
+def log_scale(intensity, scale_factor, minimum_intensity_threshold):
 	#logarithmic scale function, can be used below when creating pts.
 	return ((scale_factor * ((10.**((intensity - minimum_intensity_threshold) 
 	/ (1. - minimum_intensity_threshold))) - 1)) * (1./9.))
 
-def scale(intensity, exp=1):
+def scale(intensity, scale_factor, minimum_intensity_threshold, exp=1):
 	#scale function can be exponential, used below to make pts.
 	return (scale_factor * ((intensity - minimum_intensity_threshold) 
 	/ (1. - minimum_intensity_threshold))**exp)
 
-def init3D_shape():
+def init3D_shape(r_len, xDimen, yDimen, xDimen_len, yDimen_len, centerY_len, centerX_len, 
+len_per_pixel, scale_factor, minimum_intensity_threshold):
 	'''This creates the initial 3D shape of the hemisphere that will then be modified with
 	intensity values to create the protrusions'''
 	
@@ -277,7 +297,8 @@ def init3D_shape():
 	z_init = np.asarray(zlist)
 	return x_init, y_init, z_init
 	
-def add_function(exp=1., buffer=False, len_per_pixel=km_per_pixel):
+def add_function(len_per_pixel, xDimen, yDimen, minimum_intensity_threshold, buffer_zone, 
+centerX, centerY, image, scale_bool, r, scale_factor, exp=1., buffer=False):
 	'''This for loop goes through all the points and determines whether or not it is on the
 	hemisphere and whether or not the intensity surpasses the threshold. If not, then no 
 	multiplier is added. If yes then a multiplier is created based on the intensity of 
@@ -297,12 +318,18 @@ def add_function(exp=1., buffer=False, len_per_pixel=km_per_pixel):
 			 > (r - buffer_zone))))):
 				row.append(0)
 			 else:
-				row.append(scale(image[xpoint][ypoint], exp=exp))
+			 	if(scale_bool):
+					row.append(scale(image[xpoint][ypoint], scale_factor, 
+					minimum_intensity_threshold, exp=exp))
+				else:
+					row.append(log_scale(image[xpoint][ypoint], scale_factor, 
+					minimum_intensity_threshold))
 		add.append(row)
 	
 	return add
 	
-def final_height_addition(x_init, y_init, z_init, add):
+def final_height_addition(xDimen, yDimen, centerX, centerY, centerX_len, centerY_len, 
+r_len, r, x_init, y_init, z_init, add):
 
 	print('adding protrusions...')
 
@@ -356,70 +383,86 @@ def retrieve_image(date):
 			image[x][y] = float(image[x][y]) / 255.
 	
 	return image, header
+	
+def image_to_stl_mesh(date, r=925, base_len=100., offset_x=0, offset_y=0, 
+scale_factor_percent=0.25, minimum_intensity_threshold=0.35, buffer_zone=0., buffer=False,
+exp=1., scale_bool=True):
+	'''
+	This turns an image from the given date into a 3D printable stl file
+	
+	Parameters:
+	
+		date : str, 'YYYY/MM/DD', date of the image
+		
+		r : float, radius of the sun in px, determines size of hemisphere
+		
+		base_len : float, determines the length of the stl file base in mm
+		
+		offset_x : int, can be used to offset center of hemisphere to match a not centered
+		sun
+		
+		offset_y : int, same as x, except in y dimension
+		
+		scale_factor_percent : float, the percent of the radius that the prominences can
+		stick out
+		
+		minimum_intensity_threshold : float (0-1), the percent intensity that the point 
+		has to surpass in order to show up as a prominence and not be set to zero
+		
+		buffer_zone : int, the amount in pixels that within the hemisphere around the outer 
+		edge that is set to 0 as a buffer to prevent a potentially misshapen or warped
+		hemisphere 
+		
+		buffer : boolean, determines whether or not the buffer_zone will exist
+		
+		exp : float, the exponent that all of the intensities are taken to the power of 
+		for exponential scaling
+		
+		scale_bool : boolean, if true, the scaling is exponential (based on exp), if false, 
+		the scaling is logarithmic
+	'''
+	
+	image, header = retrieve_image(date)
+	
+	#pixel dimensions of the image
+	xDimen = image.shape[0]
+	yDimen = image.shape[1]
+	
+	len_per_pixel = base_len / xDimen
+	
+	#pixel dimensions in kilometers
+	xDimen_len = xDimen * len_per_pixel
+	yDimen_len = yDimen * len_per_pixel
+	
+	#new calculated center of sun
+	centerX = xDimen/2. - offset_x
+	centerY = yDimen/2. - offset_y
 
-km_per_pixel = km_per_pixel()
+	 #in km
+	centerX_len = centerX * len_per_pixel
+	centerY_len = centerY * len_per_pixel
+	
+	r = float(r) #determines the size of the sphere, should be radius of the sun in image
+	r_len = r * len_per_pixel #in km
 
-#len_per_pixel = 0.05 #for stl file making
-len_per_pixel = km_per_pixel #for plotting
+	scale_factor = scale_factor_percent * r_len
+	
+	x_init, y_init, z_init = init3D_shape(r_len, xDimen, yDimen, xDimen_len, yDimen_len,
+	centerY_len, centerX_len, len_per_pixel, scale_factor, minimum_intensity_threshold)
+
+	add = add_function(len_per_pixel, xDimen, yDimen, minimum_intensity_threshold, 
+	buffer_zone, centerX, centerY, image, scale_bool, r, scale_factor, exp=exp, buffer=True)
+
+	x, y, z = final_height_addition(xDimen, yDimen, centerX, centerY, centerX_len, centerY_len, 
+	r_len, r, x_init, y_init, z_init, add)
+	
+	return x, y, z
 
 date = '2007/02/02'
+r = 925.
 
-image, header = retrieve_image(date)
+image_to_stl_mesh(date, r=r, base_len=100., offset_x=30, offset_y=-30, 
+scale_factor_percent=0.25, minimum_intensity_threshold=0.35, buffer_zone=0., buffer=False,
+exp=1.3, scale_bool=True)
 
-#info from the header
-XCEN = header[0].__getitem__('XCEN')
-YCEN = header[0].__getitem__('YCEN')
-DATEOBS = header[0].__getitem__('CTIME')
-NAXIS1 = header[0].__getitem__('NAXIS1')
-NAXIS2 = header[0].__getitem__('NAXIS2')
-
-#image data is already normalized to [0.0, 1.0]
-#image = read_png('2007_02_02__12_02_58_608__HINODE_XRT_COMP.png')
-
-#pixel dimensions of the image
-xDimen = image.shape[0]
-yDimen = image.shape[1]
-
-#Can be used to offset the radius if sun is not in center of image
-offsetX = 30. #px
-offsetY = -30. #px
-
-#pixel dimensions in kilometers
-xDimen_len = xDimen * len_per_pixel
-yDimen_len = yDimen * len_per_pixel
-
-#new calculated center of sun
-centerX = xDimen/2. - offsetX
-centerY = yDimen/2. - offsetY
-
- #in km
-centerX_len = centerX * len_per_pixel
-centerY_len = centerY * len_per_pixel
-
-r = 925. #determines the size of the sphere, should be radius of the sun in image
-r_len = r * len_per_pixel #in km
-
-x_init, y_init, z_init = init3D_shape()
-
-#Bright features stand out
-
-#how much they could possibly stand out by
-scale_factor_percent = 0.25
-scale_factor = scale_factor_percent * r_len
-
-minimum_intensity_threshold = 0.35 #intensity values must exceed this in order to 
-#become protrusions. This prevents inflation to maintain spherical shape and is not
-#necessary for logarithmic and exponential scales
-
-buffer_zone = 25. #region around outside edge of hemisphere that has no protrusions to
-#prevent warping and inflating around edges
-
-add = add_function(exp=1.3, buffer=True)
-
-x, y, z = final_height_addition(x_init, y_init, z_init, add)
-
-#stl_mesh_maker(x, y, z, interval=3)
-
-ThreeDPlot(x, y, z, image)
-
-#stl_file_maker(image)
+stl_mesh_maker(x, y, z, interval=4, fname='test1.stl')
